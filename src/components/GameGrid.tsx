fbb5e07947afc605e7f5c661ab9524d07cf903fd
= <agun@duck.com>
2024-02-21 13:28:25 +0700

    Create custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,13 +1,8 @@
-import {ReactNode, useEffect, useState} from "react";
-import apiClient from "../services/api-client.ts";
+import {ReactNode} from "react";
 import {Text} from "@chakra-ui/react";
+import useGames, {Game, UseGames} from "../hooks/useGames.ts";
 
-interface Game {
-	id: number;
-	name: string;
-}
-
-interface RAWGResponse<T> {
+export interface RAWGResponse<T> {
 	count: number;
 	results: T[];
 	next: string;
@@ -15,14 +10,7 @@ interface RAWGResponse<T> {
 }
 
 const GameGrid = () => {
-	const [games, setGames] = useState<Game[]>([]);
-	const [error, setError] = useState<string>('');
-
-	useEffect(() => {
-		apiClient.get<RAWGResponse<Game>>("/games")
-			.then((res) => setGames(res.data.results))
-			.catch(err => setError(err.message));
-	}, []);
+	const {error, games}: UseGames = useGames();
 
 	return (
 		<>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,37 @@
+import {useEffect, useState} from "react";
+import apiClient from "../services/api-client.ts";
+import {RAWGResponse} from "../components/GameGrid.tsx";
+import {AxiosRequestConfig, AxiosResponse, CanceledError} from "axios";
+
+export interface Game {
+	id: number;
+	name: string;
+}
+
+export interface UseGames {
+	games: Game[];
+	error: string
+}
+
+const useGames = (): UseGames => {
+	const controller: AbortController = new AbortController();
+	const [games, setGames] = useState<Game[]>([]);
+	const [error, setError] = useState<string>('');
+
+	useEffect(() => {
+		const config: AxiosRequestConfig = {signal: controller.signal}
+
+		apiClient.get<RAWGResponse<Game>>("/games", config)
+			.then((res: AxiosResponse<RAWGResponse<Game>>) => setGames(res.data.results))
+			.catch(err => {
+				if (err instanceof CanceledError) return;
+				setError(err.message);
+			});
+
+		return () => controller.abort();
+	}, []);
+
+	return {games, error}
+}
+
+export default useGames;

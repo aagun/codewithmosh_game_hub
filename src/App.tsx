10363bbdad0c6b5c912ae1b70f26cbf0a398a099
= <agun@duck.com>
2024-02-21 11:20:42 +0700

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import {Button} from "@chakra-ui/react";
+import {Grid, GridItem, Show} from "@chakra-ui/react";
 
 function App() {
 
-  return (
-    <Button colorScheme="teal">Button</Button>
-  )
+	return (
+		<Grid templateAreas={{
+			base: `"nav" "main"`,
+			lg: `"nav nav" "aside main"`,
+		}}>
+			<GridItem area="nav" bg="teal">Navbar</GridItem>
+			<Show above="lg">
+				<GridItem area="aside" bg="yellow">Aside</GridItem>
+			</Show>
+			<GridItem area="main" bg="blue">Main</GridItem>
+		</Grid>
+	)
 }
 
 export default App

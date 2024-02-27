25dacba4853132ad6fd267ed0ce3f1bb4d4f1acb
= <agun@duck.com>
2024-02-27 12:49:17 +0700

    Refactor game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,23 +15,22 @@ const GameGrid = ({gameQuery}: Props) => {
 	const {error, data: games, isLoading}: UseDataProps<Game> = useGames(gameQuery);
 	const skeletons: number[] = [1, 2, 3, 4, 5, 6];
 
-	return (
-		<>
-			{error && <Text color="red.500">{error}</Text>}
-			<SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} spacing={6} p="10px">
-				{isLoading && skeletons.map((skeleton: number): ReactNode =>
-					<GameCardContainer key={skeleton}>
-						<GameCardSkeleton/>
-					</GameCardContainer>
-				)}
+	if (error) return <Text color="red.500">{error}</Text>;
 
-				{games.map((game: Game): ReactNode =>
-					<GameCardContainer key={game.id}>
-						<GameCard game={game}/>
-					</GameCardContainer>
-				)}
-			</SimpleGrid>
-		</>
+	return (
+		<SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} spacing={6} p="10px">
+			{isLoading && skeletons.map((skeleton: number): ReactNode =>
+				<GameCardContainer key={skeleton}>
+					<GameCardSkeleton/>
+				</GameCardContainer>
+			)}
+
+			{games.map((game: Game): ReactNode =>
+				<GameCardContainer key={game.id}>
+					<GameCard game={game}/>
+				</GameCardContainer>
+			)}
+		</SimpleGrid>
 	);
 };
 
